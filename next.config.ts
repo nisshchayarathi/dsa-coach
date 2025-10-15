d73fde49f17eedd1866199a1e64a7f917c4eb955
nisshchayarathi <nisshchayarathi111@gmail.com>
2025-10-15 18:04:20 +0000

    Fix 1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+ webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@google/genai');
+      config.externals.push('@google/generative-ai');
+      config.externals.push('@langchain/google-genai');
+      config.externals.push('@pinecone-database/pinecone');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
